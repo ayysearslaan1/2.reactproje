592be45728b4eb10aa9ebf4b74c53005ac70461b
ayysearslaan1 <114736890+ayysearslaan1@users.noreply.github.com>
2022-12-15 12:31:47 +0300

    2.REACTPROJEM
    yaşam döngüsü örneği

--- 1reactreje/src/App.js
+++ 1reactreje/src/App.js
@@ -2,19 +2,15 @@ import logo from './logo.svg';
 import user from './components/user';
 import navbar from "./components/navbar";
 import user from './components/user';
+import test from './test';
 
 import './App.css';
 
 function App() {
   return (
-    <div className='container'>
-      <navbar title= "user app2"/>
-      <hr>
-      name= "Ayşe Arslan"
-      salary="5000"
-      Department="Bilişim"
+   <>
 
-      <hr/>
+
       
     
       
---
+++ 1reactreje/src/components/test.js
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+export default function test(){
+
+
+    useEffect(() => {
+        console.log("component ronder oldu!")
+
+    }
+    )
+    useEffect(() => {
+        consele.log("component ilk yüklendiğinde çalışır")
+    }
+
+    )
+        
+
+    
+} 
+
